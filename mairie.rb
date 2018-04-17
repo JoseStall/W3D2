901303820e44720328c8b0503e7bb394f88d2e71
Alexandro <alejo_alvarez67@hotmail.com>
2018-04-17 12:29:25 +0200

    Merge branch 'master' of https://github.com/JoseStall/W3D2

--- mairie.rb
+++ mairie.rb
@@ -9,7 +9,7 @@ require "csv"
 
 #Ici, on connecte google API et on déclare ws en variable absolue avec le @
 session = GoogleDrive::Session.from_config("config.json")
-@ws = session.spreadsheet_by_key("1TWu-jicfHUX8vv8iype6G8VS1E3_GuUsmRJEFHhK_L0").worksheets[0]
+@ws = session.spreadsheet_by_key("1S3sTXhAIRYT8znscsT4BiZBRy1Vi7Jw0jZQKvET2hIc").worksheets[0]
 
 # 1er exercice, on récupére l'adresse mail d'une page demandé
 
@@ -57,7 +57,7 @@ def put_it_in_spreadsheet
 	for z in 0..@x.length
 		@ws[z+2,1] = @x[z]
 		@ws[z+2,2] = @y[z]
-		@ws[z+2,3] = "59"
+		@ws[z+2,3] = "69"
  	end
 
 	@ws.save
